8fe652e80cfbedde654141e1e50e0f1cd6091c64
Christi Savino <c.savino@ymail.com>
2016-12-11 16:42:36 -0500

    added dbURI

--- controllers/controller.js
+++ controllers/controller.js
@@ -17,8 +17,14 @@ var cheerio = require("cheerio");
 var Promise = require("bluebird");
 mongoose.Promise = Promise;
 
+//Database configuration with mongoose
+var dbURI = 'mongodb://localhost/webscraping';
+if (process.env.NODE_ENV === 'production') {
+    dbURI= "mongodb://heroku_w677159l:cn2kbl6l1cogrv4vf13g13iug8@ds133158.mlab.com:33158/heroku_w677159l";
+}
+
 // Database configuration with mongoose
-mongoose.connect("MONGODB_URI: mongodb://heroku_w677159l:cn2kbl6l1cogrv4vf13g13iug8@ds133158.mlab.com:33158/heroku_w677159l");
+mongoose.connect("dbURI");
 var db = mongoose.connection;
 
 // Show any mongoose errors
